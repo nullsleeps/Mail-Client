7a3ae04a650cb83b49ebd068928f5e759cb8d6ef
null <68474200+nullsleeps@users.noreply.github.com>
2024-11-02 10:14:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,31 +4,31 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-server = smtplib.SMTP('smtp.gmail.com', 25)
-
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()  
 server.ehlo()
 
-server.login('email@email.com', 'password') #change to your email and password
+server.login('email@email.com', 'password')
 
 msg = MIMEMultipart()
-msg['From'] = 'Null'
-msg['To'] = 'email2@email2.com'
+msg['From'] = 'your_email@gmail.com'
+msg['To'] = 'recipient_email@gmail.com'
 msg['Subject'] = 'message'
 
-with open ('message.text', 'r') as f:
+with open('message.text', 'r') as f:
     message = f.read()
-
 msg.attach(MIMEText(message, 'plain'))
 
 filename = 'water.jpg'
-attachment = open(filename, 'rb')
+with open(filename, 'rb') as attachment:
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
 
-p = MIMEBase('application', 'octet-stream')
-p.set_payload(attachment.read())
-
-encoders.encode_base64(p)
-p.add_header('Content-Disposition', f'attatchment; filename-{filename}')
-msg.attach(p)
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', f'attachment; filename="{filename}"')
+msg.attach(part)
 
 text = msg.as_string()
 server.sendmail('email@email.com', 'email2@email2.com', text)
+
+server.quit()
