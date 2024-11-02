7b354354553393be15be895676f2ca6746bca440
null <68474200+nullsleeps@users.noreply.github.com>
2024-11-02 10:17:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()  
 server.ehlo()
 
-server.login('email@email.com', 'password')
+server.login('email@email.com', 'password') #put in your email and password
 
 msg = MIMEMultipart()
-msg['From'] = 'your_email@gmail.com'
-msg['To'] = 'recipient_email@gmail.com'
-msg['Subject'] = 'message'
+msg['From'] = 'your_email@gmail.com' #put in your email
+msg['To'] = 'recipient_email@gmail.com' #put in the recievers email
+msg['Subject'] = 'message' #msg
 
-with open('message.text', 'r') as f:
+with open('something.text', 'r') as f: #edit the something.txt file to wtv u wanna send
     message = f.read()
 msg.attach(MIMEText(message, 'plain'))
 
-filename = 'water.jpg'
+filename = 'water.jpg' #change the photo to any photo or video u want
 with open(filename, 'rb') as attachment:
     part = MIMEBase('application', 'octet-stream')
     part.set_payload(attachment.read())
